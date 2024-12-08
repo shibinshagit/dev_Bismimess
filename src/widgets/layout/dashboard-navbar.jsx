f20d005e5adf5cfa85da36e56ffc56f5a9f2aebf
shibinshagit <shibinshamunna912@gmail.com>
2024-12-08 19:23:00 +0530

    notification

--- src/widgets/layout/dashboard-navbar.jsx
+++ src/widgets/layout/dashboard-navbar.jsx
@@ -20,7 +20,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { BaseUrl } from "@/constants/BaseUrl"; // If your BaseUrl is defined here
 import io from 'socket.io-client';
-const socket = io("wss://admin.bismimess.online", {
+const socket = io(BaseUrl, {
   transports: ["websocket", "polling"], // Ensure fallback support for polling
   secure: true,
 });
